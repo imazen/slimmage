e31d376f6294690fd3cba3ea273943c87ba6536a
Nathanael Jones <nathanael.jones@gmail.com>
2013-07-05 19:54:37 -0400

    Fixed console.log bug, improved diagnostic messages, and improved IE compatibility.

--- slimmage.js
+++ slimmage.js
@@ -4,7 +4,7 @@
 
     w.slimage = w.slimage || {};
     w.slimage.settings || {};
-    var log = w.window.console || function(){};
+    var log = function(){w.console && w.console.log.apply(w.console,arguments);};
     w.slimage.beginWebPTest = function(){
         if (!w.slimage.settings.serverHasWebP || w.slimage._testingWebP) return;
         w.slimage._testingWebP = true;
@@ -47,27 +47,21 @@
             if (w.slimage.webp) newSrc = newSrc.replace(/format=[a-z]+/i,"format=webp");
             img.src =  newSrc; 
             img.setAttribute("data-pixel-width", maxwidth);
+            log("Slimming: updating " + newSrc)
         }
     };
 
     w.slimage.adjustImageSrc = function (img, originalSrc) {
-        if (!img.addEventListener) {
-            img.src = originalSrc;
-            return; //Use fallback img on older IE versions.
-        }
         var wImg = img.cloneNode();
         wImg.src = "";
         wImg.style.paddingBottom = "-1px";
         wImg.removeAttribute("data-slimmage");
         wImg.removeAttribute("data-ri");
         img.parentNode.insertBefore(wImg, img);
-
-        var imgLoaded = function () {
-            wImg.removeEventListener("load", imgLoaded);
+        wImg.onload=function(){
             w.slimage.adjustImageSrcWithData(img, originalSrc, wImg);
         };
         //Load a 4,000 pixel wide image, see what the resulting true width is.
-        wImg.addEventListener("load", imgLoaded, false);
         wImg.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAD6AAAAABCAAAAADvHA58AAAACXZwQWcAAA+gAAAAAQDjne1PAAAAG0lEQVRIx+3BIQEAAAACIP+f1hkWIAUAAADuBuaLkULU/NTrAAAAAElFTkSuQmCC";
     };
 
@@ -78,8 +72,7 @@
             w.slimage.timeoutid = w.setTimeout(w.slimage.checkResponsiveImages, delay);
             return;
         }
-
-        log("Scanning for noscript tags, updating images");
+        var newImages = 0;
         //1. Copy images out of noscript tags, but hide 'src' attribute as data-src
         var n = w.slimage.nodesToArray(w.document.getElementsByTagName("noscript"));
         for (var i = 0, il = n.length; i < il; i++) {
@@ -113,6 +106,7 @@
                     }
                     ci.setAttribute("data-slimmage", true);
                     ns.parentNode.insertBefore(ci, ns);
+                    newImages++;
                 }
                 //2. Remove old noscript tags
                 ns.parentNode.removeChild(ns);
@@ -120,14 +114,17 @@
         }
 
         //3. Find images with data-slimmage and run adjustImageSrc.
+        var totalImages = 0;
         var images = w.slimage.nodesToArray(w.document.getElementsByTagName("img"));
         for (var i = 0, il = images.length; i < il; i++) {
             if (images[i].getAttribute("data-slimmage") !== null) {
                 var originalSrc = images[i].getAttribute("data-src") || images[i].src;
                 w.slimage.adjustImageSrc(images[i], originalSrc);
-                log("Slimming " + originalSrc);
+                totalImages++;
             }
         }
+
+        log("Slimmage: unfolded " + newImages + " images from noscript tags; began size calculations on " + totalImages + " images.");
     };
 
     var h = w.slimage.checkResponsiveImages;
