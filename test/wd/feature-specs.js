5c95c75abc7cc52148c09a2a669e2d8b002f4476
Josh Ponelat <josh@ponelat.com>
2014-11-05 12:22:12 +0200

    Normalise quotes to single-quote

--- test/wd/feature-specs.js
+++ test/wd/feature-specs.js
@@ -1,11 +1,11 @@
-var chai = require("chai");
-var chaiAsPromised = require("chai-as-promised");
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 chai.should();
 var verbose = true;
 
 // this.browser and this.wd are injected by the grunt-wd-mocha plugin.
-//var setup = require("./setup")(this); // setup details, such as logging and credentials
+//var setup = require('./setup')(this); // setup details, such as logging and credentials
 
 var slim = { // Slimmage defaults
   widthStep: 160
@@ -37,7 +37,7 @@ describe('slimmage', function() {
     });
 
     after(function(done) {
-      if (browser.mode === "saucelabs") {
+      if (browser.mode === 'saucelabs') {
         browser
           .quit()
           .sauceJobStatus(allPassed)
@@ -50,19 +50,19 @@ describe('slimmage', function() {
     });
 
     // Begin actual tests...
-    describe("with defaults", function(done) {
+    describe('with defaults', function(done) {
       var page; // Promise chain, once we have a loaded page
       before(function(){
-        page = browser.get("http://127.0.0.1:9999/test/feature-defaults.html")
+        page = browser.get('http://127.0.0.1:9999/test/feature-defaults.html')
       })
       after(function(){
         page.nodeify(done) // describe is finished at this point
       })
 
-      it("should load without errors", function(done) {
+      it('should load without errors', function(done) {
           page
             .title()
-            .should.become("slimmage defaults")
+            .should.become('slimmage defaults')
           .nodeify(done)
       });
 
@@ -70,15 +70,15 @@ describe('slimmage', function() {
         it('should ratchet up to 160', function(done) {
             page
               .elementByClassName('fixedsize_100') // img.max_width == 100
-              .getAttribute("src")
-              .should.become("http://z.zr.io/ri/1s.jpg?width=" + slim.widthStep) // ... final return
+              .getAttribute('src')
+              .should.become('http://z.zr.io/ri/1s.jpg?width=' + slim.widthStep)
             .nodeify(done)
         });
         it('should ratchet up to 320', function(done) {
             page
               .elementByClassName('fixedsize_200') // img.max_width == 200
-              .getAttribute("src")
-              .should.become("http://z.zr.io/ri/1s.jpg?width=" + (slim.widthStep*2))
+              .getAttribute('src')
+              .should.become('http://z.zr.io/ri/1s.jpg?width=' + (slim.widthStep*2))
             .nodeify(done)
         });
       });
