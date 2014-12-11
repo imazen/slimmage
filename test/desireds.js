0979b70f9821343b1723fff000be7a9188e40ed6
Josh Ponelat <josh@ponelat.com>
2014-12-11 11:16:03 +0200

    Add all desired browsers

--- test/desireds.js
+++ test/desireds.js
@@ -2,19 +2,37 @@
 'use strict';
 
 module.exports = {
+
     firefox: {
       browserName: 'firefox',
       platform: 'Windows',
       'screen-resolution': '1024x768'
     },
+
+    explorer6: {
+      browserName: 'internet explorer',
+      version: '6.0',
+      'screen-resolution': '1024x768'
+    },
+
     explorer: {
       browserName: 'internet explorer',
       'screen-resolution': '1024x768'
     },
+
     chrome: {
       browserName: 'chrome',
       platform: 'Linux',
       version: '38.0',
       'screen-resolution': '1024x768'
+    },
+
+    ios: {
+      'browserName': 'iphone',
+      'platform': 'OS X 10.9',
+      'version': '8.0',
+      // 'deviceName':'iPhone Simulator',
+      'device-orientation': 'portrait'
     }
+
 };
