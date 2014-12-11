3f865587fdc0659a63431cb35ded170e868a776a
Josh Ponelat <josh@ponelat.com>
2014-12-11 10:10:41 +0200

    Split wd/feature-spec into three files

---
+++ test/wd/expected.js
@@ -0,0 +1,73 @@
+// testElements
+var expected = module.exports = {};
+var port = process.env.PORT || 3000;
+
+// Slimmage defaults
+expected.slim = {
+  widthStep: 160
+};
+
+expected.calc_nearest_slim_step = function (val) {
+  return val - (val % expected.slim.widthStep) + expected.slim.widthStep;
+};
+
+expected.win_tollerance = 30; // = px; tolerance for padding/margin/window-frame
+expected.explicit_wait = 3000;
+
+// given->expected for the repeatable tests run
+expected.pages = {
+  normal: {
+    name: 'default',
+    url:'http://127.0.0.1:'+ port +'/test/feature-defaults.html',
+    title: 'slimmage defaults'
+  },
+
+  webp: {
+    name: 'webp',
+    url:'http://127.0.0.1:'+ port +'/test/feature-webp.html',
+    title: 'slimmage webp'
+  },
+};
+
+expected.desktop = {
+
+    medium: {
+      devicePixelRatio: 1,
+      size: {
+        width: 800,
+        height: 600
+      },
+      halfsize: 400, // 1334/4
+      halfsize_src: expected.calc_nearest_slim_step(400)
+    },
+
+    large: {
+      devicePixelRatio: 1,
+      size: {
+        width: 1024,
+        height: 768
+      },
+
+      halfsize: 512,
+      halfsize_src: expected.calc_nearest_slim_step(512)
+    }
+
+};
+
+expected.mobiles = {
+  // NOTE: this is not the larger 'plus' version
+  iphone6: {
+    landscape: {
+      devicePixelRatio: 2,
+      halfsize: 333, // 1334/4
+      // first factor in dpr, then round up to nearest step
+      halfsize_src: expected.calc_nearest_slim_step(333 * 2)
+    },
+    portrait: {
+      devicePixelRatio: 2,
+      halfsize: 180,
+      // first factor in dpr, then round up to nearest step
+      halfsize_src: expected.calc_nearest_slim_step(180 * 2)
+    }
+  }
+};
---
+++ test/wd/test_functions.js
@@ -0,0 +1,173 @@
+/* global describe,before,it */
+
+var e = require('./expected.js');
+var util	= require('./util.js');
+var test = module.exports = {};
+
+//--------------------------------------------------------------------------
+//---       The following are only functions, and unless called          ---
+//---       ... from within a test, they do nothing.                     ---
+//--------------------------------------------------------------------------
+
+//--------------------------------------------------------------------------
+//---    High level test suites                                          ---
+//--------------------------------------------------------------------------
+
+test.desktop = function() {
+  describe('desktop',function() {
+
+    test.changeWindowSize.call(this, e.desktop.medium.size );
+    test.loadPage.call(this, e.pages.normal );
+    test.elements.call(this, e.desktop.medium );
+    test.changeWindowSize.call(this, e.desktop.large.size );
+    test.elements.call(this, e.desktop.large );
+
+  });
+};
+
+test.mobile = function() {
+  describe('mobile',function() {
+
+    test.changeOrientation.call(this, 'PORTRAIT');
+    test.loadPage.call(this, e.pages.normal );
+    test.elements.call(this, e.mobiles.iphone6.portrait) ;
+    test.changeOrientation.call(this, 'LANDSCAPE');
+    test.elements.call(this, e.mobiles.iphone6.landscape);
+
+    test.changeOrientation.call(this, 'PORTRAIT');
+    test.loadPage.call(this, e.pages.webp );
+    test.elements.call(this, e.mobiles.iphone6.portrait) ;
+    test.changeOrientation.call(this, 'LANDSCAPE');
+    test.elements.call(this, e.mobiles.iphone6.landscape);
+
+  });
+};
+
+//--------------------------------------------------------------------------
+//---    Desktop specific                                                ---
+//--------------------------------------------------------------------------
+
+test.changeWindowSize = function(size) {
+  describe('changing to ' + size.width,function() {
+    var chain;
+    before(function() {
+      chain = this.browser
+        .setWindowSize( size.width , size.height );
+    });
+
+    it('should be the right size ('+ size.width + ')', function(done) {
+      chain
+      .getWindowSize()
+      .should.eventually.have.deep.property('width', size.width)
+      .nodeify(done);
+    });
+
+    it('should wait until the body has resized', function(done) {
+      chain
+        .waitFor(util.asserter(function(t) {
+          var a = size.width - e.win_tollerance;
+          var b = size.width + e.win_tollerance;
+          return t
+            .elementByTagName('body')
+            .getSize()
+            .then(function(size) {
+              return size.width.should.be.within(a,b);
+            });
+        }), e.explicit_wait) // repeat the above until success or timeout
+        .nodeify(done);
+    });
+
+  });
+};
+
+
+//--------------------------------------------------------------------------
+//---    Mobile specific                                                 ---
+//--------------------------------------------------------------------------
+
+test.changeOrientation  = function(value) {
+  describe('orientation',function() {
+    var chain;
+    before(function() {
+      chain = this.browser.setOrientation(value);
+    });
+
+    it('should change to ' + value, function(done) {
+      chain
+      .getOrientation()
+      .should.become(value)
+      .nodeify(done);
+    });
+
+  });
+};
+
+// ------------------------------------------------------------------------
+// Load page
+// ------------------------------------------------------------------------
+test.loadPage = function(page_details) {
+  describe(page_details.name,function() {
+    var chain;
+    before(function(){
+      chain = this.browser
+      .get(page_details.url);
+    });
+
+    it('should load the right page', function(done) {
+      chain
+      .title()
+      .should.become(page_details.title)
+      .nodeify(done);
+    });
+  });
+};
+
+test.elements = function(vals){
+  var dpr = vals.devicePixelRatio || 1;
+  var fix100_src = e.calc_nearest_slim_step(dpr * 100);
+  var fix200_src = e.calc_nearest_slim_step(dpr * 200);
+
+  describe('fixedwidth_100', function() {
+
+    it('src url should ratchet up to ' + fix100_src , function(done) {
+     this.browser
+        .elementByClassName('fixedsize_100') // img.max_width == 100px
+        .getAttribute('src')
+        .should.become('http://z.zr.io/ri/1s.jpg?width=' + fix100_src)
+        .nodeify(done);
+    });
+
+  });
+
+  describe('fixedwidth_200', function() {
+    it('src url should ratchet up to ' + fix200_src, function(done) {
+     this.browser
+        .elementByClassName('fixedsize_200') // img.max_width == 200px
+        .getAttribute('src')
+        .should.become('http://z.zr.io/ri/1s.jpg?width=' + fix200_src)
+        .nodeify(done);
+    });
+  });
+
+  describe('halfsize', function() {
+    it('should be '+ vals.halfsize +' +/-'+ e.win_tollerance +' px wide',function(done) {
+     this.browser
+        .waitFor(util.widthToBeWithin('.halfsize',
+            vals.halfsize,
+            e.win_tollerance
+          ), e.explicit_wait)
+        .nodeify(done);
+    });
+
+    it('src url should ratchet up to '+ vals.halfsize_src ,function(done) {
+     this.browser
+        .waitFor(util.asserter(function(t) {
+          return t
+            .elementByClassName('halfsize')
+            .getAttribute('src')
+            .should.become('http://z.zr.io/ri/1s.jpg?width=' + vals.halfsize_src );
+        }), e.explicit_wait) // repeat the above until success or timeout
+        .nodeify(done);
+    });
+  });
+};
