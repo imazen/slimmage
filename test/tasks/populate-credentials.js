77188cfb209c6d7a8c4cee53e9eded44922b69e2
Josh Ponelat <josh@ponelat.com>
2014-12-12 18:21:23 +0200

    Remove env.json functionality

--- test/tasks/populate-credentials.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(grunt) {
-  grunt.registerTask('populate-credentials',  function() {
-    // Put credentials into saucelabs-mocha config. Can be done after config had been set.
-    grunt.config("saucelabs-mocha.all.options.username", process.env.SAUCE_USERNAME);
-    grunt.config("saucelabs-mocha.all.options.key", process.env.SAUCE_ACCESS_KEY);
-  });
-};
