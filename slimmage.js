d4584d359f7fdb1d74fcacba0bdabd5cff977504
Nathanael Jones <nathanael.jones@gmail.com>
2015-01-14 00:33:48 -0500

    Revert max-width->width fallback. width is capricious, usleess data source. (for example, picks up size of broken image icon, or container width, non-deterministically).

--- slimmage.js
+++ slimmage.js
@@ -179,12 +179,8 @@
     };
     s['adjustImageSrc'] = function (img, previousSrc) {
 
-        var cssWidth = s.getCssPixels(img, 'max-width');
-        if (!cssWidth) { 
-          cssWidth = s.getCssPixels(img, 'width'); //Fall back to width if max-width is not available (IE6-8 are max-width allergic)
-        }
-
-        var result = s['getImageInfo'](cssWidth, 
+        var cssMaxWidth = s.getCssPixels(img, 'max-width');
+        var result = s['getImageInfo'](cssMaxWidth, 
                                        previousSrc, 
                                        img.getAttribute('data-pixel-width') | 0, 
                                        img);
