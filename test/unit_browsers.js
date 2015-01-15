af8cc2ca6b9c621b6e17ba3c59bc3d4ece2dd2f9
Nathanael Jones <nathanael.jones@gmail.com>
2015-01-15 13:54:10 -0500

    Tests: run unit tests on more browsers.

--- test/unit_browsers.js
+++ test/unit_browsers.js
@@ -3,24 +3,79 @@
 
 module.exports = {
 
-    firefox: {
-      browserName: 'firefox',
-      platform: 'Windows 8.1',
-      'screen-resolution': '1024x768'
+
+    explorer6: {
+       platform: 'Windows XP',
+       browserName: 'internet explorer',
+       version: '6'
     },
 
-    explorer: {
-      browserName: 'internet explorer',
-      version: '6',
-      platform: 'Windows XP',
-      'screen-resolution': '1024x768'
-    },
-
-    ios: {
+    ios_emulator: {
       'browserName': 'iphone',
       'platform': 'OS X 10.9',
       'version': '8.0',
       // 'deviceName':'iPhone Simulator',
       'device-orientation': 'portrait'
+    },
+
+    android4_0: {
+      browserName: 'android',
+      platform: 'Linux', 
+      version: '4.0'
+    },
+
+    chrome_linux: {
+      browserName: 'chrome',
+      platform: 'Linux',
+      version: '39'
+    },
+
+    explorer11: {
+      platform: 'Windows 8.1',
+      browserName: 'internet explorer',
+      version: '11'
+    },
+
+    firefox_windows: {
+      browserName: 'firefox',
+      platform: 'Windows'
+    },
+
+
+    safari7_mac: {
+      browserName: 'safari',
+      platform: 'OS X 10.9', 
+      version: '7',
+      deviceName: ''
+    },
+    explorer7: {
+       platform: 'Windows XP',
+       browserName: 'internet explorer',
+       version: '7'
+    },
+
+    explorer8: {
+       platform: 'Windows 7',
+       browserName: 'internet explorer',
+       version: '8'
+    },
+
+    explorer9: {
+      platform: 'Windows 7',
+      browserName: 'internet explorer',
+      version: '9'
+    },
+
+    explorer10: {
+      platform: 'Windows 8',
+      browserName: 'internet explorer',
+      version: '10'
+    },
+
+    android4_4: {
+      browserName: 'android',
+      platform: 'Linux', 
+      version: '4.4'
     }
+
 };
