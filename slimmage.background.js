456a5fad984694c9523f0bec5a555c4accff8773
Jason Prothero <protherj@gmail.com>
2015-11-06 10:39:44 -0800

    added comments, license, and note about max-width CSS requirement

--- slimmage.background.js
+++ slimmage.background.js
@@ -1,3 +1,10 @@
+/**
+ * @preserve Slimmage Background Plugin 0.4.2, use with ImageResizer. MIT/Apache2 dual licensed by Imazen and HiQ Finland and ProWorks Corporation
+ */
+
+/* We often use string instead of dot notation to keep 
+   Closure Compiler's advanced mode from breaking APIs */
+/*jshint sub:true*/
 
 (function (w) { //w==window
     // Enable strict mode
@@ -6,23 +13,23 @@
     var s =  window['slimmage'];
 	
 	s['adjustBackgroundImageSrc'] = function(elem, previousSrc) {
-        var result = s['getImageInfo'](cssMaxWidth,
+	    var cssMaxWidth = s['getCssPixels'](elem, 'max-width');             // Note: The div or element with data-slimmage-bg needs to have max-width: 100% as a part of its styles.
+	    var result = s['getImageInfo'](cssMaxWidth ? cssMaxWidth : 2000,    //       This check will fall back if max-width is not set so the image doesn't break.
                                        previousSrc,
                                        elem.getAttribute('data-pixel-width') | 0,
                                        elem);
  
         if (result){
           s.setAttr(elem,'data-pixel-width',result['data-pixel-width']);
-		  elem.style.backgroundImage = "url(" + newSrc + ")";
-	      if (!elem.style.backgroundImage) elem.style.backgroundImage = "url(" + originalSrc + ")";
-          log("Slimming: updating " + result['src']);
+          elem.style.backgroundImage = "url(" + result['src'] + ")";
+          if (!elem.style.backgroundImage) elem.style.backgroundImage = "url(" + previousSrc + ")";
 		}
     }
 
 	s['beforeAdjustSrc'] = function(){
 		if (w.document.querySelectorAll) {
-			var i, il, j, jl, k, kl;
-			var totalImages = 0;
+		    var k, kl;
+		    var totalImages = 0;
 			var images = s.nodesToArray(w.document.querySelectorAll("[data-slimmage-bg]"));
 			for (k = 0, kl = images.length; k < kl; k++) {
 				var previousSrc = images[k].getAttribute("data-slimmage-bg");
@@ -32,7 +39,6 @@
                 }
 			}
 		}
-        log("Slimmage: updated " + newImages + " images from background-image styles, checked " + totalImages + " images, changed " + changed.length + ". " + (new Date().getTime() - stopwatch) + "ms");
 	}
 	
 }(this));
